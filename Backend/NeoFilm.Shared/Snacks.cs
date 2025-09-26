f8c1931bc7c758e34656cebac1e64930d6a84394
DavidTr <davidrozotuberquia@gmail.com>
2025-09-26 17:32:04 -0500

    Organized structure

---
+++ Backend/NeoFilm.Shared/Snacks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoFilm.Shared
+{
+    public class Snacks
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public decimal UnitValue { get; set; }
+        public string Description { get; set; }
+        public bool State {  get; set; }
+    }
+}
